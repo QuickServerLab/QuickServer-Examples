5d5574e82a042cf51c7f00a5c5cc84ce8a026bc7
Akshathkumar Shetty <akshath@gmail.com>
2014-03-02 17:14:22 +0530

    update param

--- bin/TestEchoServer.bat
+++ bin/TestEchoServer.bat
@@ -1,3 +1,3 @@
 @rem args = port ip maxClient clientJunkCount clientJunkSleepInSec
-@java -cp %classpath%;.\..\lib\QuickServer.jar;.\..\dist\echoserver.jar echoserver.TestEchoServer 4123 127.0.0.1 5 50 1
+@java -cp %classpath%;.\..\lib\QuickServer.jar;.\..\dist\echoserver.jar echoserver.TestEchoServer 4123 127.0.0.1 1000 50 1
 @rem java -cp %classpath%;.\..\dist\echoserver.jar echoserver.TestEchoServerReadLine 4123 127.0.0.1
--- bin/TestEchoServer.sh
+++ bin/TestEchoServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # args = port ip maxClient clientJunkCount clientJunkSleepInSec
-# exec java -cp ./../lib/QuickServer.jar:./../dist/echoserver.jar echoserver.TestEchoServer 4123 127.0.0.1 1000 5 1
-exec java -cp ./../lib/QuickServer.jar:./../dist/echoserver.jar echoserver.TestEchoServer 4123 127.0.0.1 1000 100 180
+# exec java -cp ./../lib/QuickServer.jar:./../dist/echoserver.jar echoserver.TestEchoServer 4123 127.0.0.1 1000 50 1
+exec java -cp ./../lib/QuickServer.jar:./../dist/echoserver.jar echoserver.TestEchoServer 4123 127.0.0.1 1000 50 1
 #exec java -cp ./../dist/echoserver.jar echoserver.TestEchoServerReadLine 4123 127.0.0.1
